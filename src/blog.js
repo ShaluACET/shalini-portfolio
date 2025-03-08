55b1f5c3a376720b8ab108650947161e233d9abd
ShaluACET <shalinishalini6272@gmail.com>
2025-03-08 16:42:54 +0530

    myportfolio

--- src/blog.js
+++ src/blog.js
@@ -1,26 +1,28 @@
 import './App.css';
 function Blog() {
   return (
-    <div>
-      <div className="background">
-      <nav className="navbar">
-        <div className="logo">Shalini Blog</div>
-        <div className="menu">
-          <button className="menu-item">Home</button>
-          <button className="menu-item">About</button>
-          <button className="menu-item">Services</button>
-          <button className="menu-item">Contact</button>
+  <div className="background">
+        <nav className="navbar">
+          <div className="logo">Shalini Blog</div>
+          <div className="menu">
+            <button className="menu-item">Home</button>
+            <button className="menu-item">About</button>
+            <button className="menu-item">Services</button>
+            <button className="menu-item">Contact</button>
+          </div>
+        </nav>
+        <div>
+          <nav className="header">
+            <h5>Hey, I'M</h5>
+            <h1>R SHALINI</h1>
+            <p className="styled-paragraph">
+              A frontend-focused Web Developer building the frontend of Websites & Web
+              <br />
+              Applications that lead to the success of overall projects
+            </p>
+          </nav>
         </div>
-      </nav>
-      <div>
-      <nav className="header">
-        <h5>Hey, I'M</h5>
-        <h1>R SHALINI</h1>
-        <p className="styled-paragraph">A frontend focused Web Developer buliding the frontend of Websites & Web<br/>Appliations that leads to the success of the overall projects</p>
-      </nav>
       </div>
-    </div>
-    </div>
-  );
+      );
 }
 export default Blog;
