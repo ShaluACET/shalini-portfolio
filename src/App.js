5e82dab59044923afdc42ae05406e0777b998c8b
ShaluACET <shalinishalini6272@gmail.com>
2025-03-26 18:23:26 +0530

    myportfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Blog from './Blog.js';
+import Home from "./pages/home.js";
+// import About from "./pages/about.js";
+//import Projects from "./pages/Project.js";
+//import Contact from "./pages/contact.js";
 import Footer from './pages/footer.js';
 
 
@@ -7,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <Blog/>
+      <Home/>
+      {/* <Projects/>
+      <Contact/> */}
       <Footer/>
     </div>
   );
--- src/blog.js
+++ src/blog.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home.js";
-// import About from "./pages/about.js";
-import Projects from "./pages/projects.js";
+//import About from "./pages/about.js";
+import Projects from "./pages/Project.js";
 import Contact from "./pages/contact.js";
-import Footer from "./pages/footer.js";
 import "./App.css";
 
 function App() {
